118eeb60143146873570f988a1a1d81ff92359be
Julian <julian@britesc.com>
2025-09-22 11:04:09 +0200

    Added Helper Functions

--- ws_lvgl_9_template.ino
+++ ws_lvgl_9_template.ino
@@ -3,7 +3,7 @@
 */
 
 /*******************************************************************************
- * Start of Arduino_GFX setting
+ * Start of Arduino_GFX settings
  ******************************************************************************/
 #include <Arduino_GFX_Library.h>
 #include <lcd_defines.h>
@@ -15,28 +15,41 @@ Arduino_DataBus *bus = new Arduino_RPiPicoSPI(LCD_DC_PIN, LCD_CS_PIN, LCD_SCK_PI
 Arduino_GFX *gfx = new Arduino_ST7789(
   bus, LCD_RST_PIN, LCD_Orientation /* rotation */, true /* IPS */);
 /*******************************************************************************
- * End of Arduino_GFX setting
+ * End of Arduino_GFX settings
  ******************************************************************************/
 
 /*******************************************************************************
- * Start of Wire setting
+ * Start of Wire settings
  ******************************************************************************/
 #include <Wire.h>
 /*******************************************************************************
- * End of Wire setting
+ * End of Wire settings
  ******************************************************************************/
 
 /*******************************************************************************
- * Start of lvgl setting
+ * Start of lvgl settings
  ******************************************************************************/
 #include <lvgl.h>
 #include <bsp_cst328.h>
 /*******************************************************************************
- * End of lvgl setting
+ * End of lvgl settings
  ******************************************************************************/
 
-#define BACKGROUND RGB565_YELLOW
-#define VERSION    "0.0.9"
+/*******************************************************************************
+ * Start of sketch settings
+ ******************************************************************************/
+uint32_t screenRotation;
+uint32_t screenWidth;
+uint32_t screenHeight;
+uint32_t bufSize;
+lv_display_t *disp;
+lv_color_t *disp_draw_buf1;
+lv_color_t *disp_draw_buf2;
+#define BACKGROUND RGB565_PINK
+#define VERSION    "0.0.11"
+/*******************************************************************************
+ * End of sketch settings
+ ******************************************************************************/
 
 void setup(void)
 {
@@ -44,19 +57,8 @@ void setup(void)
   DEV_DEVICE_INIT();
 #endif
 
-  // Serial.begin(115200);
-  // // Serial.setDebugOutput(true);
-  // while(!Serial);
-  // Serial.println("WS Pico 2350 Touchscreen Template");
-  // Serial.println(VERSION);
   init_serial();
 
-  // Init Display
-  // if (!gfx->begin())
-  // {
-  //   Serial.println("gfx->begin() failed!");
-  // }
-  // gfx->fillScreen(BACKGROUND);
   init_display();
 
 #ifdef GFX_BL
@@ -73,6 +75,8 @@ void loop()
 {
  
 }
+
+
 
 void init_serial()
 {
@@ -99,3 +103,51 @@ void init_wire()
   Wire1.begin();
   Wire1.setClock(I2C_SPEED);
 }  
+
+#if LV_USE_LOG != 0
+void my_print(lv_log_level_t level, const char *buf)
+{
+  LV_UNUSED(level);
+  Serial.println(buf);
+  Serial.flush();
+}
+#endif
+
+uint32_t millis_cb(void)
+{
+  return millis();
+}
+
+/* LVGL calls it when a rendered image needs to copied to the display*/
+void my_disp_flush(lv_display_t *disp, const lv_area_t *area, uint8_t *px_map)
+{
+  uint32_t w = lv_area_get_width(area);
+  uint32_t h = lv_area_get_height(area);
+
+  gfx->draw16bitRGBBitmap(area->x1, area->y1, (uint16_t *)px_map, w, h);
+  /*Call it to tell LVGL you are ready*/
+  lv_disp_flush_ready(disp);
+}
+
+/*Read the touchpad*/
+void my_touchpad_read(lv_indev_t *indev, lv_indev_data_t *data)
+{
+  bsp_cst328_data_t cst328_data;
+  bsp_cst328_read();
+  if (bsp_cst328_get_touch_data(&cst328_data))
+  {
+    data->state = LV_INDEV_STATE_PR;
+
+    /*Set the coordinates*/
+    data->point.x = cst328_data.coords[0].x;
+    data->point.y = cst328_data.coords[0].y;
+  }
+  else
+  {
+    data->state = LV_INDEV_STATE_REL;
+  }
+}
+
+/* ------------------------------------------------------------------------ */
+/* ------------------------------------------------------------------------ */
+/* ------------------------------------------------------------------------ */
